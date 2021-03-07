7809a761ca7c9cd599a1175014d48a3ce694cc26
Spiros42 <bucekpet@seznam.cz>
2021-03-07 14:07:22 +0100

    added content

---
+++ script.js
@@ -0,0 +1,143 @@
+// ------------------------------------------------------
+// Canvas and context setup
+// ------------------------------------------------------
+// ------------------------------------------------------
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
+let hue = 0;
+const mouse = {x: undefined, y: undefined, }
+
+// ------------------------------------------------------
+// Listeners
+// ------------------------------------------------------
+// ------------------------------------------------------
+// Listener on event resize
+window.addEventListener("resize", function()
+{
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+
+// Listener on event mouse click
+canvas.addEventListener("click", function(event)
+{
+    mouse.x = event.x;
+    mouse.y = event.y;
+    // Create x particles
+    for (let i = 0; i < 100; i++)
+    {
+        particlesArray.push(new Particle());
+    }
+})
+
+// Listener on event mouse move
+canvas.addEventListener("mousemove", function(event)
+{
+    mouse.x = event.x;
+    mouse.y = event.y;
+    // Create x particles
+    for (let i = 0; i < 5; i++)
+    {
+        particlesArray.push(new Particle());
+    }
+})
+
+// ------------------------------------------------------
+// Particle class
+// ------------------------------------------------------
+// ------------------------------------------------------
+class Particle
+{
+    constructor()
+    {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = Math.random() * 8 + 1;
+        this.speedX = Math.random() * 20 - 10;
+        this.speedY = Math.random() * 20 - 10;
+        this.color = "hsl( "+ hue + ", 100%, 50%)";
+    }
+    // Particle update function
+    update()
+    {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        // Decrease size if over threshold or set 0
+        if (this.size > 0.3) this.size -= 0.3;
+        else this.size = 0;
+    }
+    // Particle draw function
+    draw()
+    {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size ,0 , Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+// ------------------------------------------------------
+// Functions
+// ------------------------------------------------------
+// ------------------------------------------------------
+// // Initialization of particles array
+// function init_array() 
+// {
+//     for (let i = 0; i < 250; i++)
+//     {
+//         particlesArray.push(new Particle());
+//     }    
+// }
+// init_array();
+
+// Updating and drawing particles
+function handleParticles()
+{
+    for (let i = 0; i < particlesArray.length; i++)
+    {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+        // Calculating distances
+        for (let j = i; j < particlesArray.length; j++)
+        {
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            // When particles are closer than 100px make line between them
+            if (distance < 100)
+            {
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color;
+                ctx.lineWidth = particlesArray[i].size;
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx.stroke();
+            }
+        }
+        // Deleting particles with size under threshold
+        if (particlesArray[i].size <= 0.3)
+        {
+            particlesArray.splice(i, 1);
+            console.log(particlesArray.length);
+            i--;
+        }
+    }
+}
+
+// Animate function
+function animate()
+{
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(0, 0, 0, 0.02)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    hue += 3;
+
+    requestAnimationFrame(animate);    
+}
+
+animate();
